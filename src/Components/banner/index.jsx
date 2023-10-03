920edb9e85501eb7e70ff2457d3a22ccda39abcb
Matheus Pinheiro da Cunha <131805878+mpinheirodev@users.noreply.github.com>
2023-10-03 14:33:30 -0300

    Update index.jsx banner import

--- src/Components/banner/index.jsx
+++ src/Components/banner/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './banner.css'
+import bannerImg from '../../src/assets/HeaderHeader_total.png'
 
 
 function Banner(){
     return (
         <header className="banner">
-            <img src="./src/assets/HeaderHeader_total.png" />
+            <img src={bannerImg} />
         </header>
     )
 }
